37e45484cd056a3c9e6b6a670d40f5dd5ef55c1c
Jagadish <pjagadish83@gmail.com>
2020-06-20 17:50:36 +0530

    Entity Framework Database First Approach

---
+++ EFTutorials/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolDBEntities())
+            {
+                var std = new Student()
+                {
+                    StudentName = "Jagadish"                    
+                };
+                context.Students.Add(std);
+
+                int statuscount = context.SaveChanges();
+
+                if(statuscount > 0)
+                {
+                    Console.WriteLine("Record Inserted Successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Insert Record Failed");
+                }
+            }
+
+
+            Console.WriteLine("Hello");
+        }
+    }
+}
